0f25393120f33f2398e2566a28227d5e38725576
jelenal1 <jabbruh10@gmail.com>
2023-12-03 16:18:52 +0100

    fix: links

--- app/components/Sidenav.tsx
+++ app/components/Sidenav.tsx
@@ -3,49 +3,49 @@ import Link from "next/link";
 import { MdClose } from "react-icons/md";
 
 export default function Sidenav({
-	open,
-	setOpen,
+  open,
+  setOpen,
 }: {
-	open: boolean;
-	setOpen: (open: boolean) => void;
+  open: boolean;
+  setOpen: (open: boolean) => void;
 }) {
-	return (
-		<div
-			className={
-				open
-					? "fixed top-0 right-0 z-10 h-screen w-screen bg-slate-100 bg-opacity-60 items-center"
-					: "fixed top-0 right-0 z-10 h-screen w-screen bg-slate-100 bg-opacity-60 items-center hidden"
-			}
-		>
-			<div className="flex flex-col items-center">
-				<button className="mt-32 mb-1" onClick={() => setOpen(false)}>
-					<MdClose className="text-[#5B2205] text-3xl" />
-				</button>
-				<Link
-					href={"/"}
-					className="my-2 text-[#5B2205] text-xl focus-within:text-[#5b220596]"
-				>
-					Appartments
-				</Link>
-				<Link
-					href={"/"}
-					className="my-2 text-[#5B2205] text-xl focus-within:text-[#5b220596]"
-				>
-					About Us
-				</Link>
-				<Link
-					href={"/"}
-					className="my-2 text-[#5B2205] text-xl focus-within:text-[#5b220596]"
-				>
-					Contact
-				</Link>
-				<Link
-					href={"/"}
-					className="my-2 text-[#5B2205] text-xl focus-within:text-[#5b220596]"
-				>
-					Login
-				</Link>
-			</div>
-		</div>
-	);
+  return (
+    <div
+      className={
+        open
+          ? "fixed top-0 right-0 z-10 h-screen w-screen bg-slate-100 bg-opacity-60 items-center"
+          : "fixed top-0 right-0 z-10 h-screen w-screen bg-slate-100 bg-opacity-60 items-center hidden"
+      }
+    >
+      <div className="flex flex-col items-center">
+        <button className="mt-32 mb-1" onClick={() => setOpen(false)}>
+          <MdClose className="text-[#5B2205] text-3xl" />
+        </button>
+        <Link
+          href={"/apartments"}
+          className="my-2 text-[#5B2205] text-xl focus-within:text-[#5b220596]"
+        >
+          Apartments
+        </Link>
+        <Link
+          href={"/about"}
+          className="my-2 text-[#5B2205] text-xl focus-within:text-[#5b220596]"
+        >
+          About Us
+        </Link>
+        <Link
+          href={"/contact"}
+          className="my-2 text-[#5B2205] text-xl focus-within:text-[#5b220596]"
+        >
+          Contact
+        </Link>
+        <Link
+          href={"/login"}
+          className="my-2 text-[#5B2205] text-xl focus-within:text-[#5b220596]"
+        >
+          Login
+        </Link>
+      </div>
+    </div>
+  );
 }
