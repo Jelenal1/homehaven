2e37609f1a23a808c4360eceb4d70c5c17ecc8dc
jelenal1 <jabbruh10@gmail.com>
2023-12-03 19:45:30 +0100

    feat: dynamic route

---
+++ app/apartments/[id]/page.tsx
@@ -0,0 +1,34 @@
+import { mockingData } from "@/app/apartments/page";
+import Image from "next/image";
+import { LiaCoinsSolid } from "react-icons/lia";
+
+export default function page({ params }: { params: { id: string } }) {
+  const object = mockingData.find(
+    (object) => object.id === parseInt(params.id),
+  );
+
+  if (!object) {
+    return <div>Apartment not found</div>;
+  }
+
+  return (
+    <div className="flex flex-col min-h-screen items-center text-[#5B2205]">
+      <h1 className="text-3xl my-5">{object.title}</h1>
+      {object.image ? (
+        <Image
+          src={object.image}
+          width={500}
+          height={500}
+          alt={object.title}
+          className="mx-auto"
+        />
+      ) : null}
+      <div className="flex items-center justify-center">
+        <span>CHF</span>
+        <LiaCoinsSolid />
+        <h2 className="ml-1">{object.price}</h2>
+      </div>
+      <p className="break-words">{object.description}</p>
+    </div>
+  );
+}
--- app/apartments/page.tsx
+++ app/apartments/page.tsx
@@ -1,49 +1,49 @@
 import Card from "../components/Card";
 import Footer from "./../components/Footer";
 
-export default function page() {
-  const mockingData = [
-    {
-      id: 1,
-      title: "Apartment 1",
-      description: "This is an apartment",
-      price: 100,
-      image: "/Homehaven.png",
-    },
-    {
-      id: 2,
-      title: "Apartment 2",
-      description: "This is an apartment",
-      price: 100,
-      image: "/Homehaven.png",
-    },
-    {
-      id: 3,
-      title: "Apartment 3",
-      description: "This is an apartment",
-      price: 100,
-      image: "/Homehaven.png",
-    },
-    {
-      id: 4,
-      title: "Apartment 4",
-      description: "This is an apartment",
-      price: 100,
-      image: "/Homehaven.png",
-    },
-    {
-      id: 5,
-      title: "Apartment 5",
-      description: "This is an apartment",
-      price: 100,
-      image: "/Homehaven.png",
-    },
-  ];
+export const mockingData = [
+  {
+    id: 1,
+    title: "Apartment 1",
+    description: "This is an apartment",
+    price: 100,
+    image: "/Homehaven.png",
+  },
+  {
+    id: 2,
+    title: "Apartment 2",
+    description: "This is an apartment",
+    price: 100,
+    image: "/Homehaven.png",
+  },
+  {
+    id: 3,
+    title: "Apartment 3",
+    description: "This is an apartment",
+    price: 100,
+    image: "/Homehaven.png",
+  },
+  {
+    id: 4,
+    title: "Apartment 4",
+    description: "This is an apartment",
+    price: 100,
+    image: "/Homehaven.png",
+  },
+  {
+    id: 5,
+    title: "Apartment 5",
+    description: "This is an apartment",
+    price: 100,
+    image: "/Homehaven.png",
+  },
+];
 
+export default function page() {
   return (
     <>
       <div className="flex flex-col min-h-screen items-center">
-        <h1 className="text-3xl my-5">Apartments</h1>
+        <h1 className="text-3xl my-5 text-[#5B2205]">Apartments</h1>
         <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-2 mx-9">
           {mockingData.map((object) => (
             <Card object={object} key={object.id} />
