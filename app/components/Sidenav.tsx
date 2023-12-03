ea6db78884f6e345525b443e6b5b4dbcb91c8da5
jelenal1 <jabbruh10@gmail.com>
2023-12-03 19:13:15 +0100

    style: css animation

--- app/components/Sidenav.tsx
+++ app/components/Sidenav.tsx
@@ -13,8 +13,8 @@ export default function Sidenav({
     <div
       className={
         open
-          ? "fixed top-0 right-0 z-10 h-screen w-screen bg-slate-100 bg-opacity-60 items-center"
-          : "fixed top-0 right-0 z-10 h-screen w-screen bg-slate-100 bg-opacity-60 items-center hidden"
+          ? "fixed top-0 right-0 z-10 h-screen w-screen bg-slate-100 bg-opacity-60 items-center transition-all ease-in"
+          : "fixed top-0 right-0 z-10 h-screen w-screen bg-slate-100 bg-opacity-60 opacity-0 invisible items-center transition-all ease-out duration-200"
       }
     >
       <div className="flex flex-col items-center">
@@ -23,24 +23,28 @@ export default function Sidenav({
         </button>
         <Link
           href={"/apartments"}
+          onClick={() => setOpen(false)}
           className="my-2 text-[#5B2205] text-xl focus-within:text-[#5b220596]"
         >
           Apartments
         </Link>
         <Link
           href={"/about"}
+          onClick={() => setOpen(false)}
           className="my-2 text-[#5B2205] text-xl focus-within:text-[#5b220596]"
         >
           About Us
         </Link>
         <Link
           href={"/contact"}
+          onClick={() => setOpen(false)}
           className="my-2 text-[#5B2205] text-xl focus-within:text-[#5b220596]"
         >
           Contact
         </Link>
         <Link
           href={"/login"}
+          onClick={() => setOpen(false)}
           className="my-2 text-[#5B2205] text-xl focus-within:text-[#5b220596]"
         >
           Login
