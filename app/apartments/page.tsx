775c603d1ad41d0089d4056079f3b996bd8a34eb
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2023-12-03 15:26:45 +0100

    Working on cards component

--- app/apartments/page.tsx
+++ app/apartments/page.tsx
@@ -1,3 +1,20 @@
+import Card from "../components/Card";
+
 export default function page() {
-  return <div>page</div>;
+  const mockingData = [
+    {
+      id: 1,
+      title: "Apartment 1",
+      description: "This is an apartment",
+      price: 100,
+      image: "/Homehaven.png",
+    },
+  ];
+
+  return (
+    <div className="flex flex-col min-h-screen items-center">
+      <h1>Apartments</h1>
+      <Card object={mockingData[0]} />
+    </div>
+  );
 }
---
+++ app/components/Card.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+
+export default function Card({
+  object,
+}: {
+  object: {
+    id: number;
+    title: string;
+    description: string;
+    price: number | null | undefined;
+    image: string | null | undefined;
+  };
+}) {
+  return (
+    <div className="w-full">
+      <div className="text-[#5B2205] border border-black mx-9 text-center">
+        {object.image ? (
+          <Image
+            src={object.image}
+            width={200}
+            height={200}
+            alt={object.title}
+            className="mx-auto"
+          />
+        ) : null}
+        <h1>{object.title}</h1>
+        <h2>{object.price}</h2>
+        <p>{object.description}</p>
+      </div>
+    </div>
+  );
+}
