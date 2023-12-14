505cf3125e12ac6bb066317e732fe52c569aa520
jelenal1 <jabbruh10@gmail.com>
2023-12-14 22:36:55 +0100

    perf: use client works better

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,43 +2,30 @@
 import { useState } from "react";
 
 export default function page() {
+  const [Subject, setSubject] = useState("");
+  const [Message, setMessage] = useState("");
   return (
     <div className="mx-9 flex min-h-screen flex-col items-center text-[#5B2205]">
+      <h1 className="my-5 text-3xl">Contact Us</h1>
       <form
-        className="mt-[15vh] flex w-full max-w-[600px] flex-col items-center"
+        className="flex w-full max-w-[600px] flex-col items-center"
         onSubmit={(e) => {
           e.preventDefault();
-          const Prename = e.currentTarget.prename.value;
-          const Sirname = e.currentTarget.sirname.value;
-          const Subject = e.currentTarget.subject.value;
-          const Message = e.currentTarget.message.value;
           const recipient = "homehaven@homehaven.com";
-          if (!Prename || !Sirname || !Subject || !Message) return;
+          if (Subject === "" || Message === "") return;
           window.location.href = `mailto:${recipient}?subject=${Subject}&body=${Message}`;
+          setSubject("");
+          setMessage("");
         }}
       >
-        <label htmlFor="prename" className="text-sm">
-          Prename
-        </label>
-        <input
-          type="text"
-          name="prename"
-          className="mb-1 h-6 w-full rounded pl-0.5 outline-[2.5px] outline-[#5B2205] focus:outline"
-        />
-        <label htmlFor="sirname" className="text-sm">
-          Sirname
-        </label>
-        <input
-          type="text"
-          name="sirname"
-          className="mb-1 h-6 w-full rounded pl-0.5 outline-[2.5px] outline-[#5B2205] focus:outline"
-        />
         <label htmlFor="subject" className="text-sm">
           Subject
         </label>
         <input
           type="text"
           name="subject"
+          value={Subject}
+          onChange={(e) => setSubject(e.target.value)}
           className="mb-1 h-6 w-full rounded pl-0.5 outline-[2.5px] outline-[#5B2205] focus:outline"
         />
         <label htmlFor="message" className="text-sm">
@@ -46,6 +33,8 @@ export default function page() {
         </label>
         <textarea
           name="message"
+          value={Message}
+          onChange={(e) => setMessage(e.target.value)}
           className="h-20 w-full rounded pl-0.5 outline-[2.5px] outline-[#5B2205] focus:outline"
         />
         <input
