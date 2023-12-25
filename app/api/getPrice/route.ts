a8ac42d18ac47928879a72995abfabbb77dfd3ab
jelenal1 <jabbruh10@gmail.com>
2023-12-25 17:00:16 +0100

    feat: getPrice route

---
+++ app/api/getPrice/route.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
+
+export async function GET(request: NextRequest) {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+  const params = request.nextUrl.searchParams;
+  const products = await stripe.products.list();
+  const product = products.data.find((o) => o.name === params.get("name"));
+  if (!product) {
+    return NextResponse.json({ error: "Product not found" }, { status: 404 });
+  }
+  const price = await stripe.prices.retrieve(product.default_price as string);
+  return NextResponse.json({
+    price_in_cents: price.unit_amount,
+  });
+}
