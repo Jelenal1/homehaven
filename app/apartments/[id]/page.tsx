20945771b98ac7f1b125a702de6fe11bc4348df7
jelenal1 <jabbruh10@gmail.com>
2023-12-25 17:08:55 +0100

    feat: getPrice function

--- app/apartments/[id]/page.tsx
+++ app/apartments/[id]/page.tsx
@@ -2,7 +2,17 @@ import { mockingData } from "@/app/apartments/page";
 import { LiaCoinsSolid } from "react-icons/lia";
 import CarouselImages from "../../_components/CarouselImages";
 
-export default function page({ params }: { params: { id: string } }) {
+const getPrice = async (name: string) => {
+  const priceResponse = await fetch(
+    `http://localhost:3000/api/getPrice?name=${name}`,
+  );
+
+  const price_data = await priceResponse.json();
+  const price = price_data.price_in_cents / 100;
+  return price;
+};
+
+export default async function page({ params }: { params: { id: string } }) {
   const object = mockingData.find(
     (object) => object.id === parseInt(params.id),
   );
@@ -11,6 +21,8 @@ export default function page({ params }: { params: { id: string } }) {
     return <div>Apartment not found</div>;
   }
 
+  const price = await getPrice(object.title);
+
   return (
     <div className="mx-9 flex min-h-screen flex-col text-[#5B2205]">
       <h1 className="mx-auto my-5 text-3xl">{object.title}</h1>
@@ -18,7 +30,7 @@ export default function page({ params }: { params: { id: string } }) {
       <div className="flex items-center justify-center">
         <span>CHF</span>
         <LiaCoinsSolid />
-        <h2 className="ml-1">{object.price}</h2>
+        <h2 className="ml-1">{price}</h2>
       </div>
       {object.description.split(`\n`).map((sentence, index) => (
         <p className="mx-auto my-2 max-w-[1000px] text-justify" key={index}>
