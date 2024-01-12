5d68354785bacffc4c15aebcd225c4843a0ae560
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2024-01-12 14:42:36 +0100

    fix: try again

--- app/_components/ServerFunctions.ts
+++ app/_components/ServerFunctions.ts
@@ -1,6 +1,6 @@
 export const getPrice = async (name: string) => {
   const parsedName = encodeURIComponent(name);
-  const priceResponse = await fetch(`/api/getPrice?name=${parsedName}`);
+  const priceResponse = await fetch(`/api/getPrice?name=${name}`);
 
   const price_data = await priceResponse.json();
   const price = price_data.price_in_cents / 100;
@@ -9,7 +9,7 @@ export const getPrice = async (name: string) => {
 
 export const getPriceId = async (name: string) => {
   const parsedName = encodeURIComponent(name);
-  const priceResponse = await fetch(`/api/getPrice?name=${parsedName}`);
+  const priceResponse = await fetch(`/api/getPrice?name=${name}`);
   const price_data = await priceResponse.json();
   return price_data.price_id;
 };
