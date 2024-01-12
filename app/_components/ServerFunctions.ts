b4ce6a6ff7f5ef23b5c06f82df2e42d9956ee817
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2024-01-12 13:57:31 +0100

    chore: links

--- app/_components/ServerFunctions.ts
+++ app/_components/ServerFunctions.ts
@@ -1,7 +1,8 @@
 export const getPrice = async (name: string) => {
-  const priceResponse = await fetch(
-    `homehaven-liard.vercel.app/api/getPrice?name=${name}`,
-  );
+  const baseUrl = "https://homehaven-liard.vercel.app/api/getPrice";
+  const query = encodeURIComponent(name);
+  const url = `${baseUrl}?name=${query}`;
+  const priceResponse = await fetch(url);
 
   const price_data = await priceResponse.json();
   const price = price_data.price_in_cents / 100;
@@ -9,9 +10,10 @@ export const getPrice = async (name: string) => {
 };
 
 export const getPriceId = async (name: string) => {
-  const priceResponse = await fetch(
-    `homehaven-liard.vercel.app/api/getPrice?name=${name}`,
-  );
+  const baseUrl = "https://homehaven-liard.vercel.app/api/getPrice";
+  const query = encodeURIComponent(name);
+  const url = `${baseUrl}?name=${query}`;
+  const priceResponse = await fetch(url);
   const price_data = await priceResponse.json();
   return price_data.price_id;
 };
