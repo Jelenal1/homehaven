1c3536b0ae3335eb2da5250efa7c774cfda81313
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2024-01-24 13:01:02 +0100

    I containerized the project

--- app/api/getProducts/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import Stripe from "stripe";
-
-export async function GET(request: NextRequest) {
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
-  const products = await stripe.products.list();
-  return NextResponse.json(products);
-}
