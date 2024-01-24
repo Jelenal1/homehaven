90fdb35c9ddb2c76684b8e8dfa183e7e1ba31b9f
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2024-01-24 09:55:58 +0100

    try to get it work

--- app/_components/ServerFunctions.ts
+++ app/_components/ServerFunctions.ts
@@ -1,12 +1,16 @@
 export const getPrice = async (name: string) => {
   const parsedName = encodeURIComponent(name);
-  const priceResponse = await fetch(
-    `https://homehaven-backend-express.onrender.com/getPrice?name=${parsedName}`,
-  );
+  try {
+    const priceResponse = await fetch(
+      `https://homehaven-backend-express.onrender.com/getPrice?name=${parsedName}`,
+    );
 
-  const price_data = await priceResponse.json();
-  const price = price_data.price_in_cents / 100;
-  return price;
+    const price_data = await priceResponse.json();
+    const price = price_data.price_in_cents / 100;
+    return price;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const getPriceId = async (name: string) => {
