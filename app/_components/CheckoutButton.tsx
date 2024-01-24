124484ef60bd3e0540dba57848ccd556ff9f0022
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2024-01-24 10:40:11 +0100

    wrong url

--- app/_components/CheckoutButton.tsx
+++ app/_components/CheckoutButton.tsx
@@ -7,7 +7,7 @@ export default function CheckoutButton({ priceId }: { priceId: string }) {
 
   const checkOut = async () => {
     const response = await fetch(
-      "https://homehaven-backend-express.onrender.com/checkout",
+      "https://homehaven-backend-express.onrender.com:10000/checkout",
       {
         method: "POST",
         headers: {
